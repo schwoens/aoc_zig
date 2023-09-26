47589739c15c7fc681990fe148b0458c7199a70e
schwoens <schwoens@protonmail.com>
2023-09-26 09:13:39 +0200

    complete 2022 day 4 part 2

--- 2022/day4/day4.zig
+++ 2022/day4/day4.zig
@@ -7,6 +7,8 @@ const input = @embedFile("input.txt");
 pub fn main() !void {
     const part1_res = try part1(input.len, input);
     print("Part 1: {}\n", .{part1_res});
+    const part2_res = try part2(input.len, input);
+    print("Part 2: {}\n", .{part2_res});
 }
 
 fn part1(comptime len: usize, in: *const [len:0]u8) !u32 {
@@ -36,8 +38,41 @@ fn part1(comptime len: usize, in: *const [len:0]u8) !u32 {
     return count;
 }
 
+fn part2(comptime len: usize, in: *const [len:0]u8) !u32 {
+    var lines = std.mem.tokenizeAny(u8, in, "\n");
+    var line = lines.next();
+    var count: u32 = 0;
+    while (line != null) : (line = lines.next()) {
+        var pair = std.mem.splitAny(u8, line.?, ",");
+        var range1 = [_]u8{0} ** 2;
+        var range2 = [_]u8{0} ** 2;
+
+        var range1_it = std.mem.splitAny(u8, pair.next().?, "-");
+        var range2_it = std.mem.splitAny(u8, pair.next().?, "-");
+
+        range1[0] = try std.fmt.parseInt(u8, range1_it.next().?, 10);
+        range1[1] = try std.fmt.parseInt(u8, range1_it.next().?, 10);
+        range2[0] = try std.fmt.parseInt(u8, range2_it.next().?, 10);
+        range2[1] = try std.fmt.parseInt(u8, range2_it.next().?, 10);
+
+        const range1_overlaps_range2 = (range1[0] >= range2[0] and range1[0] <= range2[1]) or (range1[1] >= range2[0] and range1[1] <= range2[1]);
+        const range2_overlaps_range1 = (range2[0] >= range1[0] and range2[0] <= range1[1]) or (range2[1] >= range1[0] and range2[1] <= range1[1]);
+
+        if (range1_overlaps_range2 or range2_overlaps_range1) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
 test "Part 1" {
     const expected: u32 = 2;
     const actual = try part1(test_input.len, test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2" {
+    const expected: u32 = 4;
+    const actual = try part2(test_input.len, test_input);
     try std.testing.expectEqual(expected, actual);
 }
