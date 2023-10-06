92e17bc67d091b9e5fb831b51d06b151f7423025
schwoens <schwoens@protonmail.com>
2023-10-06 11:26:21 +0200

    complete 2022 day 5 part 2

--- 2022/day5/day5.zig
+++ 2022/day5/day5.zig
@@ -7,6 +7,8 @@ const test_input = @embedFile("test_input.txt");
 pub fn main() !void {
     const part1_res = try part1(input.len, input);
     print("Part 1: {s}\n", .{part1_res});
+    const part2_res = try part2(input.len, input);
+    print("Part 2: {s}\n", .{part2_res});
 }
 
 fn part1(comptime len: usize, in: *const [len:0]u8) ![9:0]u8 {
@@ -78,8 +80,88 @@ fn part1(comptime len: usize, in: *const [len:0]u8) ![9:0]u8 {
     return result;
 }
 
+fn part2(comptime len: usize, in: *const [len:0]u8) ![9:0]u8 {
+    var file = std.mem.tokenizeSequence(u8, in, "\n\n");
+
+    var stack_lines = std.mem.tokenizeAny(u8, file.next().?, "\n");
+    var instruction_lines = std.mem.tokenizeAny(u8, file.next().?, "\n");
+
+    // initialize stacks
+    var stacks = [_]std.ArrayList(u8){undefined} ** 9;
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    for (0..stacks.len) |i| {
+        stacks[i] = std.ArrayList(u8).init(allocator);
+    }
+
+    defer for (stacks) |stack| {
+        stack.deinit();
+    };
+
+    // fill stacks with initial state
+
+    var stack_line = stack_lines.next();
+    while (stack_line != null) : (stack_line = stack_lines.next()) {
+        var i: usize = 0;
+        while (i + 1 + i * 3 < stack_line.?.len) : (i += 1) {
+            const label = stack_line.?[i + 1 + i * 3];
+            if (std.ascii.isAlphabetic(label)) {
+                try stacks[i].insert(0, label);
+            }
+        }
+    }
+
+    // compute the instructions
+
+    var instruction = instruction_lines.next();
+    var crane = std.ArrayList(u8).init(allocator);
+    while (instruction != null) : (instruction = instruction_lines.next()) {
+        var instr_parts = std.mem.tokenizeAny(u8, instruction.?, " ");
+        _ = instr_parts.next();
+        const amount = try std.fmt.parseInt(usize, instr_parts.next().?, 10);
+        _ = instr_parts.next();
+        const from = try std.fmt.parseInt(usize, instr_parts.next().?, 10);
+        _ = instr_parts.next();
+        const to = try std.fmt.parseInt(usize, instr_parts.next().?, 10);
+
+        for (0..amount) |_| {
+            if (stacks[from - 1].getLastOrNull() != null) {
+                try crane.append(stacks[from - 1].pop());
+            }
+        }
+
+        while (crane.getLastOrNull() != null) {
+            try stacks[to - 1].append(crane.pop());
+        }
+    }
+
+    // read top labels from left to right
+
+    var result = [_:0]u8{0} ** 9;
+
+    outer: for (0..stacks.len) |i| {
+        if (stacks[i].items.len < 1) {
+            break :outer;
+        }
+        var top = stacks[i].pop();
+        while (top == 0) : (top = stacks[i].pop()) {}
+        result[i] = top;
+    }
+
+    return result;
+}
+
 test "Part 1" {
     const expected: [9:0]u8 = .{ 'C', 'M', 'Z' } ++ .{0} ** 6;
     const actual = try part1(test_input.len, test_input);
+    try std.testing.expectEqualStrings(&expected, &actual);
+}
+
+test "Part 2" {
+    const expected: [9:0]u8 = .{ 'M', 'C', 'D' } ++ .{0} ** 6;
+    const actual = try part2(test_input.len, test_input);
     try std.testing.expectEqualStrings(&expected, &actual);
 }
