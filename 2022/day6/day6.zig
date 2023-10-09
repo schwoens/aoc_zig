f8ee6f3e1b12d97e6c380fcd17c615da9d51eb6d
schwoens <schwoens@protonmail.com>
2023-10-09 21:33:31 +0200

    complete 2022 day 6 part 2

--- 2022/day6/day6.zig
+++ 2022/day6/day6.zig
@@ -7,6 +7,9 @@ const test_input = @embedFile("test_input.txt");
 pub fn main() void {
     const part1_res = part1(input.len, input);
     print("Part 1: {}\n", .{part1_res});
+
+    const part2_res = part2(input.len, input);
+    print("Part 2: {}\n", .{part2_res});
 }
 
 fn part1(comptime len: usize, in: *const [len:0]u8) usize {
@@ -26,8 +29,31 @@ fn part1(comptime len: usize, in: *const [len:0]u8) usize {
     return 0;
 }
 
+fn part2(comptime len: usize, in: *const [len:0]u8) usize {
+    for (0..len - 14) |i| {
+        var unique = outer: for (in[i .. i + 14], i..) |char, j| {
+            for (in[j + 1 .. i + 14]) |c| {
+                if (char == c) {
+                    break :outer false;
+                }
+            }
+        } else true;
+
+        if (unique) {
+            return i + 14;
+        }
+    }
+    return 0;
+}
+
 test "Part 1" {
     const expected: usize = 7;
     const actual = part1(test_input.len, test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2" {
+    const expected: usize = 19;
+    const actual = part2(test_input.len, test_input);
     try std.testing.expectEqual(expected, actual);
 }
