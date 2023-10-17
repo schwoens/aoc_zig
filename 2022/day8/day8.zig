689329d6387a2bf27ea6166fc1366738d897ed05
schwoens <schwoens@protonmail.com>
2023-10-17 13:27:46 +0200

    complete 2022 day 8 part 2

--- 2022/day8/day8.zig
+++ 2022/day8/day8.zig
@@ -7,6 +7,8 @@ const test_input = @embedFile("test_input.txt");
 pub fn main() !void {
     const part1_res = try part1(input.len, input);
     print("Part 1: {}\n", .{part1_res});
+    const part2_res = try part2(input.len, input);
+    print("Part 2: {}\n", .{part2_res});
 }
 
 fn part1(comptime len: usize, in: *const [len:0]u8) !usize {
@@ -42,6 +44,42 @@ fn part1(comptime len: usize, in: *const [len:0]u8) !usize {
     return visible_count;
 }
 
+fn part2(comptime len: usize, in: *const [len:0]u8) !usize {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    var grid: std.ArrayList(std.ArrayList(u8)) = std.ArrayList(std.ArrayList(u8)).init(allocator);
+    defer {
+        for (grid.items) |list| {
+            list.deinit();
+        }
+        grid.deinit();
+    }
+
+    try grid.append(std.ArrayList(u8).init(allocator));
+    const trimmed_in = std.mem.trim(u8, in, "\n");
+    for (trimmed_in) |char| {
+        if (char == '\n') {
+            try grid.append(std.ArrayList(u8).init(allocator));
+            continue;
+        }
+        try grid.items[grid.items.len - 1].append(try std.fmt.charToDigit(char, 10));
+    }
+
+    var max_score: usize = 0;
+    for (0..grid.items.len) |y| {
+        for (0..grid.items[y].items.len) |x| {
+            const score = getScenicScore(x, y, &grid);
+            if (score > max_score) {
+                max_score = score;
+            }
+        }
+    }
+    return max_score;
+}
+
 fn isVisible(x: usize, y: usize, grid: *std.ArrayList(std.ArrayList(u8))) bool {
     const height = grid.items[y].items[x];
     var north = true;
@@ -71,8 +109,62 @@ fn isVisible(x: usize, y: usize, grid: *std.ArrayList(std.ArrayList(u8))) bool {
     return north or south or west or east;
 }
 
+fn getScenicScore(x: usize, y: usize, grid: *std.ArrayList(std.ArrayList(u8))) usize {
+    if (x == 0 or y == 0 or x == grid.items[y].items.len - 1 or y == grid.items.len - 1) {
+        return 0;
+    }
+    const height = grid.items[y].items[x];
+    var north: usize = 0;
+    var i: usize = y - 1;
+    while (i > 0) : (i -= 1) {
+        north += 1;
+        if (grid.items[i].items[x] >= height) {
+            break;
+        }
+        if (i <= 1) {
+            north += 1;
+            break;
+        }
+    }
+    var south: usize = 0;
+    i = y + 1;
+    while (i < grid.items.len) : (i += 1) {
+        south += 1;
+        if (grid.items[i].items[x] >= height) {
+            break;
+        }
+    }
+    var west: usize = 0;
+    i = x - 1;
+    while (i > 0) : (i -= 1) {
+        west += 1;
+        if (grid.items[y].items[i] >= height) {
+            break;
+        }
+        if (i <= 1) {
+            west += 1;
+            break;
+        }
+    }
+    var east: usize = 0;
+    i = x + 1;
+    while (i < grid.items[y].items.len) : (i += 1) {
+        east += 1;
+        if (grid.items[y].items[i] >= height) {
+            break;
+        }
+    }
+    return north * south * west * east;
+}
+
 test "Part 1" {
     const expected: usize = 21;
     const actual = try part1(test_input.len, test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2" {
+    const expected: usize = 8;
+    const actual = try part2(test_input.len, test_input);
     try std.testing.expectEqual(expected, actual);
 }
