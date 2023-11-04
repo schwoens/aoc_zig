39bf5f91f5428709766fae8fcfdf7f04d3a3c162
schwoens <schwoens@protonmail.com>
2023-11-04 11:37:08 +0100

    complete 2022 day 9 part 2

--- 2022/day9/day9.zig
+++ 2022/day9/day9.zig
@@ -3,10 +3,14 @@ const print = std.debug.print;
 
 const input = @embedFile("input.txt");
 const test_input = @embedFile("test_input.txt");
+const test_input2 = @embedFile("test_input2.txt");
 
 pub fn main() !void {
     const part1_res = try part1(input.len, input);
     print("Part 1: {}\n", .{part1_res});
+
+    const part2_res = try part2(input.len, input);
+    print("Part 2: {}\n", .{part2_res});
 }
 
 fn part1(comptime len: usize, in: *const [len:0]u8) !usize {
@@ -60,6 +64,51 @@ fn part1(comptime len: usize, in: *const [len:0]u8) !usize {
     return tail_memory.count();
 }
 
+fn part2(comptime len: usize, in: *const [len:0]u8) !usize {
+    var rope = [_]struct { isize, isize }{.{ 0, 0 }} ** 10;
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var tail_memory = std.AutoHashMap(struct { isize, isize }, void).init(allocator);
+    defer tail_memory.deinit();
+
+    try tail_memory.put(rope[9], {});
+
+    var lines = std.mem.tokenizeAny(u8, in, "\n");
+    var line = lines.next();
+    while (line != null) : (line = lines.next()) {
+        var split = std.mem.tokenizeAny(u8, line.?, " ");
+
+        const dir = split.next().?[0];
+        const steps = try std.fmt.parseInt(usize, split.next().?, 10);
+
+        for (0..steps) |i| {
+            _ = i;
+            switch (dir) {
+                'R' => rope[0][0] += 1,
+                'D' => rope[0][1] += 1,
+                'L' => rope[0][0] -= 1,
+                'U' => rope[0][1] -= 1,
+                else => unreachable,
+            }
+
+            for (1..10) |knot| {
+                if (!isTouching(&rope[knot - 1], &rope[knot])) {
+                    var dx = rope[knot - 1][0] - rope[knot][0];
+                    var dy = rope[knot - 1][1] - rope[knot][1];
+
+                    rope[knot][0] += std.math.sign(dx);
+                    rope[knot][1] += std.math.sign(dy);
+
+                    try tail_memory.put(rope[9], {});
+                }
+            }
+        }
+    }
+    return tail_memory.count();
+}
 fn isTouching(head: *struct { isize, isize }, tail: *struct { isize, isize }) bool {
     var y: isize = -1;
     var x: isize = -1;
@@ -83,5 +132,17 @@ test "isTouching" {
 test "Part 1" {
     const expected: usize = 13;
     const actual = try part1(test_input.len, test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2.1" {
+    var expected: usize = 1;
+    var actual = try part2(test_input.len, test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2.2" {
+    var expected: usize = 36;
+    var actual = try part2(test_input2.len, test_input2);
     try std.testing.expectEqual(expected, actual);
 }
