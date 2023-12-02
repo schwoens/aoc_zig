e87ae1b8e32543877a5fff2adbb0962e54a27570
schwoens <schwoens@protonmail.com>
2023-12-02 22:33:53 +0100

    complete 2023 day 2 part 2

--- 2023/day2/day2.zig
+++ 2023/day2/day2.zig
@@ -8,6 +8,7 @@ const test_input = @embedFile("test_input.txt");
 
 pub fn main() void {
     print("Part 1: {}\n", .{part1(input.len, input)});
+    print("Part 2: {}\n", .{part2(input.len, input)});
 }
 
 fn part1(comptime len: usize, in: *const [len:0]u8) usize {
@@ -15,9 +16,9 @@ fn part1(comptime len: usize, in: *const [len:0]u8) usize {
 
     var lines = std.mem.tokenizeAny(u8, in, "\n");
     while (lines.next()) |line| {
-        const collon_index = indexOf(u8, line, ":").?;
-        const id = parseInt(usize, line[5..collon_index], 10) catch unreachable;
-        var cubes = std.mem.tokenizeAny(u8, line[collon_index + 1 ..], ",;");
+        const colon_index = indexOf(u8, line, ":").?;
+        const id = parseInt(usize, line[5..colon_index], 10) catch unreachable;
+        var cubes = std.mem.tokenizeAny(u8, line[colon_index + 1 ..], ",;");
         while (cubes.next()) |cube| {
             var cube_split = std.mem.tokenizeAny(u8, cube, " ");
             const amount = parseInt(usize, cube_split.next().?, 10) catch unreachable;
@@ -36,7 +37,30 @@ fn part1(comptime len: usize, in: *const [len:0]u8) usize {
     return id_sum;
 }
 
-const Color = enum {
+fn part2(comptime len: usize, in: *const [len:0]u8) usize {
+    var power_sum: usize = 0;
+
+    var lines = std.mem.tokenizeAny(u8, in, "\n");
+    while (lines.next()) |line| {
+        var required_cubes = [_]usize{ 0, 0, 0 };
+        const colon_index = indexOf(u8, line, ":").?;
+        var cubes = std.mem.tokenizeAny(u8, line[colon_index + 1 ..], ",;");
+        while (cubes.next()) |cube| {
+            var cube_split = std.mem.tokenizeAny(u8, cube, " ");
+            const amount = parseInt(usize, cube_split.next().?, 10) catch unreachable;
+            const color = std.meta.stringToEnum(Color, cube_split.next().?).?;
+
+            const arr_index = @intFromEnum(color);
+            if (amount > required_cubes[arr_index]) {
+                required_cubes[arr_index] = amount;
+            }
+        }
+        power_sum += required_cubes[0] * required_cubes[1] * required_cubes[2];
+    }
+    return power_sum;
+}
+
+const Color = enum(usize) {
     red,
     green,
     blue,
@@ -45,5 +69,11 @@ const Color = enum {
 test "Part 1" {
     const expected: usize = 8;
     const actual = part1(test_input.len, test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2" {
+    const expected: usize = 2286;
+    const actual = part2(test_input.len, test_input);
     try std.testing.expectEqual(expected, actual);
 }
