a33b7a2a7d5a04d33dd03f934572b5cdb975dc51
schwoens <schwoens@protonmail.com>
2024-01-19 10:35:37 +0100

    complete 2023 day 6 part 2

--- 2023/day6/day6.zig
+++ 2023/day6/day6.zig
@@ -5,7 +5,8 @@ const input = @embedFile("input.txt");
 const test_input = @embedFile("test_input.txt");
 
 pub fn main() !void {
-    print("Part 1: {}", .{try part1(input)});
+    print("Part 1: {}\n", .{try part1(input)});
+    print("Part 2: {}\n", .{try part2(input)});
 }
 
 fn part1(in: []const u8) !usize {
@@ -34,8 +35,47 @@ fn part1(in: []const u8) !usize {
     return product;
 }
 
+fn part2(in: []const u8) !usize {
+    var area = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = area.allocator();
+
+    var lines = std.mem.tokenizeAny(u8, in, "\n");
+    const time_line = lines.next().?;
+    const distance_line = lines.next().?;
+
+    var times = time_line[(std.mem.indexOf(u8, time_line, ":").? + 1)..];
+    var distances = distance_line[(std.mem.indexOf(u8, distance_line, ":").? + 1)..];
+
+    const replaced_time_size = std.mem.replacementSize(u8, times, " ", "");
+    const replaced_distance_size = std.mem.replacementSize(u8, distances, " ", "");
+
+    var replaced_time = try allocator.alloc(u8, replaced_time_size);
+    var replaced_distance = try allocator.alloc(u8, replaced_distance_size);
+
+    _ = std.mem.replace(u8, times, " ", "", replaced_time);
+    _ = std.mem.replace(u8, distances, " ", "", replaced_distance);
+
+    var time = try std.fmt.parseInt(usize, replaced_time, 10);
+    var distance = try std.fmt.parseInt(usize, replaced_distance, 10);
+
+    var possibilities: usize = 0;
+    var time_pressed: usize = 1;
+    while (time_pressed < time) : (time_pressed += 1) {
+        if (time_pressed * (time - time_pressed) > distance) {
+            possibilities += 1;
+        }
+    }
+    return possibilities;
+}
+
 test "Part 1" {
     const expected: usize = 288;
     const actual = try part1(test_input);
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "Part 2" {
+    const expected: usize = 71503;
+    const actual = try part2(test_input);
     try std.testing.expectEqual(expected, actual);
 }
